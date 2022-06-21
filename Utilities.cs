9c3248b7517c8388b3a9a434054b16706607d934
itsmeft24 <rishabh.tumminkatti@gmail.com>
2022-06-21 12:27:23 -0400

    Move byteswapping code into Utilities.

--- Utilities.cs
+++ Utilities.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.Serialization;
 using System.Windows.Forms;
@@ -8,6 +9,31 @@ namespace BlackOps2SoundStudio
 {
     static class Utilities
     {
+        public static ulong ByteswapU64(ulong value)
+        {
+            return (value & 0x00000000000000FFUL) << 56 | (value & 0x000000000000FF00UL) << 40 |
+                   (value & 0x0000000000FF0000UL) << 24 | (value & 0x00000000FF000000UL) << 8 |
+                   (value & 0x000000FF00000000UL) >> 8 | (value & 0x0000FF0000000000UL) >> 24 |
+                   (value & 0x00FF000000000000UL) >> 40 | (value & 0xFF00000000000000UL) >> 56;
+        }
+
+        public static uint ByteswapU32(uint value)
+        {
+            return (value & 0x000000FFU) << 24 | (value & 0x0000FF00U) << 8 |
+                   (value & 0x00FF0000U) >> 8 | (value & 0xFF000000U) >> 24;
+        }
+
+        public static byte[] ByteswapU24(uint value)
+        {
+            var ret = BitConverter.GetBytes(value);
+            Array.Reverse(ret);
+            return ret.Skip(1).ToArray();
+        }
+
+        public static ushort ByteswapU16(ushort value)
+        {
+            return (ushort)((value & 0xFFU) << 8 | (value & 0xFF00U) >> 8);
+        }
         public static bool SameSourceOrDestination(Stream first, Stream second)
         {
             if (first == second)
